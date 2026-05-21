e4fb4d7e41aaf4c63fc3103d7af7c6c969666b6c
itcba <itcbachemical23@gmail.com>
2026-05-21 11:30:49 +0700

    Toilet complete

--- config/superadmin.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE saranaprasarana.users MODIFY COLUMN role enum('Admin','User','superadmin') DEFAULT 'User';
-INSERT INTO saranaprasarana.users (username, pass, role) VALUES ('superadmin', '$2y$10$fVIpgBoNpk55E1D3Z2X93OhuWyGiPrnh2KbjvEkwcUMCZyuHglHcC', 'superadmin') ON DUPLICATE KEY UPDATE pass='$2y$10$fVIpgBoNpk55E1D3Z2X93OhuWyGiPrnh2KbjvEkwcUMCZyuHglHcC', role='superadmin';
--- config/update_foto.sql
+++
@@ -1,1 +0,0 @@
-UPDATE saranaprasarana.checklist_toilet SET foto = 'uploads/sample_toilet.png' WHERE toilet_id = 1 AND tahun = 2026 AND bulan = 5;
